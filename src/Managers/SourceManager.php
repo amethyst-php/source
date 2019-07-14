63891b3fc3e3a35e9a9405e7766da10623ad446c
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/SourceManager.php
+++ src/Managers/SourceManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Source newEntity()
+ * @method \Amethyst\Schemas\SourceSchema getSchema()
+ * @method \Amethyst\Repositories\SourceRepository getRepository()
+ * @method \Amethyst\Serializers\SourceSerializer getSerializer()
+ * @method \Amethyst\Validators\SourceValidator getValidator()
+ * @method \Amethyst\Authorizers\SourceAuthorizer getAuthorizer()
+ */
 class SourceManager extends Manager
 {
     use ConfigurableManager;
